c905a7183a96e1a2adbf68475839964757c951c8
Arno Hoefnagels <hoefnagelsarno@gmail.com>
2025-03-19 13:18:05 +0100

    Added two particle teesting for forceUpdater

--- Tests/ForcesTester.cs
+++ Tests/ForcesTester.cs
@@ -1,4 +1,5 @@
 using OpenTK.Graphics.OpenGL4;
+using OpenTK.Mathematics;
 using DustCollector.GameEngine;
 using DustCollector.GameEngine.Shaders;
 namespace DustCollector.Tests;
@@ -35,12 +36,63 @@ public sealed class ForceTester
         velocityUpdater.bufferLocations.Add(1, GameEngine.Buffer.forcesFuture);
 
         velocityUpdater.SetInt("offSetX", 0);
+        velocityUpdater.SetInt("particleCount", N);
 
-        velocityUpdater.Dispatch1D(N);
+        velocityUpdater.Dispatch1D(N * (N - 1) / 2);
 
         //Check results:
         float[] forcesOut = bufferHandler.GetBufferData(GameEngine.Buffer.forcesFuture, 3 * N * (N - 1));
         CollectionAssert.AreEqual(forcesOut, forcesOutGoal);
     }
+    public static void FourParticles(TestParams testParams)
+    {
+        //Make sure GL context is correct and compile shader:
+        testParams.window.MakeCurrent();
+        GL.UseProgram(testParams.program);
+        var bufferHandler = new BufferHandler();
 
+        int N = 4;
+        var velocityUpdater = new ComputeShader(Paths.FORCEUPDATERPATH, bufferHandler);
+        Assert.IsNotNull(velocityUpdater);
+
+        //Initial positions and velocities:
+        //                   |   P1  |    P2  |    P3   |   P4     |
+        float[] positions = [1, 1, 0, -1, 1, 0, 1, -1, 0, -1, -1, 0];
+        float[] forcesIn = new float[3 * N * (N - 1)];
+        float[] forcesOutGoal = new float[3 * N * (N - 1)];
+
+        Vector3 force;
+        Vector3 positionOut;
+        Vector3 positionIn;
+        Vector3 distance;
+        for (int particleOut = 0; particleOut < N; particleOut++)
+        {
+            force = new Vector3(0, 0, 0);
+            positionOut = new Vector3(positions[3 * particleOut], positions[3 * particleOut + 1], positions[3 * particleOut + 2]);
+            for (int particleIn = 0; particleIn < N; particleIn++)
+            {
+                positionOut = new Vector3(positions[3 * particleOut], positions[3 * particleOut + 1], positions[3 * particleOut + 2]);
+                distance = new Vector3(positions[3 * particleOut])
+            }
+        }
+
+
+
+
+        //Create shader buffers and run simulation:
+        bufferHandler.CreateStorageBuffer(GameEngine.Buffer.positionsCurrent, positions, BufferUsageHint.StreamDraw);
+        bufferHandler.CreateStorageBuffer(GameEngine.Buffer.forcesFuture, forcesIn, BufferUsageHint.StreamDraw);
+
+        velocityUpdater.bufferLocations.Add(0, GameEngine.Buffer.positionsCurrent);
+        velocityUpdater.bufferLocations.Add(1, GameEngine.Buffer.forcesFuture);
+
+        velocityUpdater.SetInt("offSetX", 0);
+        velocityUpdater.SetInt("particleCount", N);
+
+        velocityUpdater.Dispatch1D(N * (N - 1) / 2);
+
+        //Check results:
+        float[] forcesOut = bufferHandler.GetBufferData(GameEngine.Buffer.forcesFuture, 3 * N * (N - 1));
+        CollectionAssert.AreEqual(forcesOut, forcesOutGoal);
+    }
 }
